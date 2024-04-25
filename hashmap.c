dc38d4751dfab91e78fc30f772cdc77cc45a81d2
Michelleeeh <163461939+Michelleeeh@users.noreply.github.com>
2024-04-25 01:09:02 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -125,10 +125,10 @@ Pair * nextMap(HashMap * map) {
     while(map->current!=-1) {
       if(map->buckets[map->current]!=NULL && map->buckets[map->current]->key!=NULL {
         return map->buckets[map->current];
-      } 
+      }
       map->current++;
       
       
     }
-    return NULL;
+    return bucket[2]->value;
 }
