d244877bc6243805d51ea2a6ee124b42ba06d979
Michelleeeh <163461939+Michelleeeh@users.noreply.github.com>
2024-04-25 01:05:57 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -102,10 +102,19 @@ Pair * searchMap(HashMap * map,  char * key) {
         }
         posicion = (posicion+1)%map->capacity;
     }
+  return NULL;
 }
 
 Pair * firstMap(HashMap * map) {
-
+    if(map==NULL) return NULL;
+    while(map->current!=-1) {
+      if(map->buckets[map->current]!=NULL && map->buckets[map->current]->key!=NULL) {
+        return map->buckets[map->current];
+        
+      }
+      map->current++;
+      
+    }
     return NULL;
 }
 
